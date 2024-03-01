432eaf7ec01b7f1d55bc35d4d00d4cd589589b38
Asuka <1626928042@qq.com>
2024-03-01 14:34:49 +0800

    新增员工

--- sky-server/src/main/java/com/asuka/SkyServerApplication.java
+++ sky-server/src/main/java/com/asuka/SkyServerApplication.java
@@ -1,13 +1,29 @@
 package com.asuka;
 
+import com.asuka.dto.EmployeeLoginDTO;
+import com.asuka.result.Result;
+import com.asuka.vo.EmployeeLoginVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.PostMapping;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @author Asuka
+ */
 
 @SpringBootApplication
+@EnableTransactionManagement
+@Slf4j
 public class SkyServerApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkyServerApplication.class, args);
+		log.info("服务成功启动...");
 	}
 
 }
---
+++ sky-server/src/main/java/com/asuka/controller/admin/EmployeeController.java
@@ -0,0 +1,91 @@
+package com.asuka.controller.admin;
+
+import com.asuka.constant.JwtClaimsConstant;
+import com.asuka.dto.EmployeeDTO;
+import com.asuka.entity.Employee;
+import com.asuka.properties.JwtProperties;
+import com.asuka.service.EmployeeService;
+import com.asuka.utils.JwtUtil;
+import com.asuka.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.asuka.result.Result;
+import com.asuka.dto.EmployeeLoginDTO;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/admin/employee")
+@RestController
+@Slf4j
+@Api(tags = "员工相关接口")
+public class EmployeeController {
+
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+
+
+    @PostMapping("/login")
+    @ApiOperation(value = "登录")
+    public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoingDTO){
+        log.info("员工登录:{}",employeeLoingDTO);
+
+        Employee employee = employeeService.login(employeeLoingDTO);
+
+        //jwt
+        Map<String,Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.EMP_ID,employee.getId());
+        String token = JwtUtil.createJWT(
+                jwtProperties.getAdminSecretKey(),
+                jwtProperties.getAdminTtl(),
+                claims);
+
+        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
+                .id(employee.getId())
+                .userName(employee.getUsername())
+                .name(employee.getName())
+                .token(token)
+                .build();
+
+        return Result.success(employeeLoginVO);
+    }
+
+    @PostMapping("/logout")
+    @ApiOperation("员工退出")
+    public Result<String> logout(){
+        return Result.success();
+    }
+
+
+    @PostMapping
+    @ApiOperation(value = "插入")
+    public Result insertEmployee(@RequestBody EmployeeDTO employeeDTO){
+        log.info("当前请求接口线程id: {} ",Thread.currentThread().getId());
+
+        log.info("插入员工,{}",employeeDTO);
+
+        employeeService.insert(employeeDTO);
+
+        return Result.success();
+    }
+
+
+
+
+
+
+
+
+}
